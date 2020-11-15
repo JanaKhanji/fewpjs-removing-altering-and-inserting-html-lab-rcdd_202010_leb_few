76af4855d5871a2f28f01b66f268cce336ce8e55
Jana Khanji <jana.khanji@ieee.org>
2020-11-15 14:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here!
 document.querySelector("#main").remove();
+<<<<<<< HEAD
 
 let newHeader = document.createElement('h1');
 newHeader.id = "victory";
 newHeader.innerHTML='Jana is the champion';
 document.querySelector(body).appendChild(newHeader);
+=======
+div=document.createElement("div");
+>>>>>>> d7e684c64e50bf368f1b6e093d41fcaacedc4deb
